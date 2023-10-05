2560238cf8ea98ec4c8185209bf863372362c94f
IbRuslan <ribragimov2003@gmail.com>
2023-10-05 23:24:03 +0500

    added post request images

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from 'mongoose'
+import multer from 'multer'
 import {loginValidation, registerValidation} from './validations/auth.js'
 import checkAuth from "./utils/checkAuth.js";
 import {authMe, login, register} from "./controllers/UserController.js";
@@ -15,17 +16,35 @@ mongoose.connect('mongodb+srv://admin:ibruslan@cluster0.kwrz95k.mongodb.net/blog
     })
 
 const app = express()
-app.use(express.json())
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({storage})
+
+app.use(express.json())
+app.use('/upload', express.static('uploads'))
 
 app.get('/auth/me', checkAuth, authMe)
 app.post('/auth/login', loginValidation, login)
 app.post('/auth/register', registerValidation, register)
 
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`
+    })
+})
+
 app.get('/posts', PostController.getAll)
 app.get('/posts/:id', PostController.getOne)
 app.post('/posts', checkAuth, postCreateValidation, PostController.create)
-app.patch('/posts/:id', checkAuth, PostController.updatePost)
+app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.updatePost)
 app.delete('/posts/:id', checkAuth, PostController.remove)
 
 
