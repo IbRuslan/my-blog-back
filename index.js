d35f4643d56b431d4366524df0bdea68162fd6f0
IbRuslan <ribragimov2003@gmail.com>
2023-10-05 20:50:14 +0500

    added auth validation and login, register request

--- index.js
+++ index.js
@@ -1,26 +1,114 @@
 import express from "express";
-import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
+import {registerValidation} from './validations/auth.js'
+import {validationResult} from 'express-validator'
+import bcrypt from "bcrypt";
+import UserModel from './models/User.js'
+import jwt from "jsonwebtoken";
 
-mongoose.connect('mongodb+srv://admin:ibruslan@cluster0.kwrz95k.mongodb.net/?retryWrites=true&w=majority')
-    .then(()=> {console.log('DB ok')})
-    .catch((err) => {console.log(`DB ${err}`)})
+mongoose.connect('mongodb+srv://admin:ibruslan@cluster0.kwrz95k.mongodb.net/blog?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('DB ok')
+    })
+    .catch((err) => {
+        console.log(`DB ${err}`)
+    })
 
 const app = express()
 app.use(express.json())
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.post('/auth/login', (req, res) => {
-    const token = jwt.sign({
-        email: req.body.email
-    }, 'secret123')
 
-    res.json({
-        success: true,
-        token
+app.get('/',
+    (req, res) => {
+        res.send('Hello World!')
     })
-})
+app.get('/auth/me',
+    (req, res) => {
+        try {
+
+        } catch {
+
+        }
+    }
+)
+app.post('/auth/login',
+    async (req, res) => {
+        try {
+            const user = await UserModel.findOne({email: req.body.email})
+            if (!user) {
+                return res.status(404).json({
+                    message: "Пользователь не найден"
+                });
+            }
+
+            const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+            if (!isValidPass) {
+                return res.status(404).json({
+                    message: "Логин или пароль не подходят"
+                })
+            }
+
+            const token = jwt.sign(
+                {
+                    _id: user._id,
+                },
+                'secterId',
+                {
+                    expiresIn: '30d'
+                }
+            );
+
+            const {passwordHash, ...userData} = user._doc
+
+            res.json({...userData, token})
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({
+                message: 'Не удалось авторизоваться'
+            })
+        }
+    }
+)
+app.post('/auth/register', registerValidation,
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json(errors.array())
+            }
+
+            const password = req.body.password
+            const salt = await bcrypt.genSalt(10)
+            const hash = await bcrypt.hash(password, salt)
+
+            const doc = new UserModel({
+                avatarUrl: req.body.avatarUrl,
+                fullName: req.body.fullName,
+                email: req.body.email,
+                passwordHash: hash
+            })
+
+            const user = await doc.save();
+
+            const token = jwt.sign(
+                {
+                    _id: user._id,
+                },
+                'secterId',
+                {
+                    expiresIn: '30d'
+                }
+            );
+
+            const {passwordHash, ...userData} = user._doc
+
+            res.json({...userData, token})
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({
+                message: 'Не удалось зарегистрироваться'
+            })
+        }
+    })
 
 app.listen(4444, (err) => {
     if (err) {
---
+++ validations/auth.js
@@ -0,0 +1,8 @@
+import {body} from 'express-validator'
+
+export const registerValidation = [
+    body('email', 'Неверный формат почты').isEmail(),
+    body('password', 'Пароль должен состоять из минимум 5 символов').isLength({min: 5}),
+    body('fullName', 'Имя должно состоять из минимум 4 символов').isLength({min: 4}),
+    body('avatarUrl', 'Неверная сыллка на аватарку').optional().isURL()
+]
