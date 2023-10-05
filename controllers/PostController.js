5c5f23614ea0b6967bad95bef8725ce87b7ace65
IbRuslan <ribragimov2003@gmail.com>
2023-10-05 23:00:16 +0500

    added CRUD request for posts

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,10 +1,10 @@
 import PostModel from './../models/Post.js'
 
-export const getAll = async (req, res) => {
+const getAll = async (req, res) => {
     try {
-        const posts = await PostModel.find()
+        const posts = await PostModel.find().populate('user').exec()
         res.json(posts)
-    } catch(err) {
+    } catch (err) {
         console.log(err)
         res.status(500).json({
             message: 'Не удалось получить статьи'
@@ -12,7 +12,32 @@ export const getAll = async (req, res) => {
     }
 }
 
-export const create = async (req, res) => {
+const getOne = async (req, res) => {
+    try {
+        const postId = req.params.id
+
+        const doc = await PostModel.findOneAndUpdate(
+            {_id: postId},
+            {$inc: {viewsCount: 1}},
+            {returnDocument: 'after'}
+        ).exec();
+
+        if (!doc) {
+            return res.status(404).json({
+                message: 'Статья не найдена'
+            });
+        }
+
+        res.json(doc);
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось получить статью'
+        })
+    }
+}
+
+const create = async (req, res) => {
     try {
         const doc = new PostModel({
             title: req.body.title,
@@ -25,7 +50,7 @@ export const create = async (req, res) => {
         const post = await doc.save();
 
         res.json(post)
-    } catch(err) {
+    } catch (err) {
         console.log(err)
         res.status(500).json({
             message: 'Не удалось создать статью'
@@ -33,4 +58,54 @@ export const create = async (req, res) => {
     }
 }
 
-export const PostController = {create}
+const updatePost = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        await PostModel.updateOne(
+            {_id: postId},
+            {
+                title: req.body.title,
+                text: req.body.text,
+                imageUrl: req.body.imageUrl,
+                tags: req.body.tags,
+                user: req.body.user
+            }
+        )
+
+        res.json({
+            success: true
+        })
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось обновить статью'
+        })
+    }
+}
+
+const remove = async (req, res) => {
+    try {
+        const postId = req.params.id
+
+        const doc = await PostModel.findOneAndDelete({_id: postId}).exec();
+
+        if (!doc) {
+            return res.status(404).json({
+                message: 'Статья не найдена'
+            });
+        }
+
+        res.json({
+            success: true
+        });
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Не удалось удалить статью'
+        })
+    }
+}
+
+export const PostController = {create, getAll, getOne, updatePost, remove}
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {loginValidation, registerValidation} from './validations/auth.js'
 import checkAuth from "./utils/checkAuth.js";
 import {authMe, login, register} from "./controllers/UserController.js";
 import {PostController} from "./controllers/PostController.js";
+import {postCreateValidation} from "./validations/post.js";
 
 mongoose.connect('mongodb+srv://admin:ibruslan@cluster0.kwrz95k.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => {
@@ -21,7 +22,11 @@ app.get('/auth/me', checkAuth, authMe)
 app.post('/auth/login', loginValidation, login)
 app.post('/auth/register', registerValidation, register)
 
-app.post('/posts', checkAuth, PostController.create)
+app.get('/posts', PostController.getAll)
+app.get('/posts/:id', PostController.getOne)
+app.post('/posts', checkAuth, postCreateValidation, PostController.create)
+app.patch('/posts/:id', checkAuth, PostController.updatePost)
+app.delete('/posts/:id', checkAuth, PostController.remove)
 
 
 app.listen(4444, (err) => {
